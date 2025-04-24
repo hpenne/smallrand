5d951c2e30338983102e1f25de899f6b20fd4a6b
Helge Penne <hpenne@gmail.com>
2025-04-24 21:22:43 +0200

    Refactored to make DefaultRng a proper type.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
 #![forbid(unsafe_code)]
 extern crate core;
 
+mod defaultrng;
 mod devices;
 mod ranges;
 mod rngs;
 mod smallrng;
 mod xoshiro;
 
+pub use defaultrng::DefaultRng;
 #[cfg(all(unix, feature = "std"))]
 pub use devices::DevUrandom;
 #[cfg(all(not(unix), feature = "std"))]
@@ -15,11 +17,3 @@ pub use devices::RandomDevice;
 pub use rngs::Rng;
 pub use smallrng::SmallRng;
 pub use xoshiro::Xoshiro256pp;
-
-/// This is the type alias for the default PRNG.
-/// It is currently not cryptographically secure, but if such an algorithm
-/// is added later, it will be used as the `DefaultRng`.
-/// The algorithm may change at any time, so if your
-/// code depends on the algorithm staying the same then you should
-/// use a specific algorithm instead.
-pub type DefaultRng = Xoshiro256pp;
