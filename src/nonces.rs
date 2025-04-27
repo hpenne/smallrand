1545039266d17f2ac38ec408c630077d28f05c58
hpenne <hpenne@gmail.com>
2025-04-27 17:10:19 +0200

    Improved nonce generation by adding the DefaultHasher and a global counter as additional sources. (#22)
    This fixes #20.

---
+++ src/nonces.rs
@@ -0,0 +1,48 @@
+//! This module implements a function to generate nonces for
+//! use in algorithm that need this.
+
+#[cfg(feature = "std")]
+use core::ops::BitXor;
+use core::sync::atomic::{AtomicU64, Ordering};
+#[cfg(feature = "std")]
+use std::collections::hash_map::DefaultHasher;
+#[cfg(feature = "std")]
+use std::hash::{Hash, Hasher};
+
+// Define a global static atomic counter
+static NONCE_COUNTER: AtomicU64 = AtomicU64::new(0);
+
+#[cfg(feature = "std")]
+pub fn nonce_u128() -> [u8; 8] {
+    // Get the time, and XOR seconds with nanoseconds:
+    let duration_since_epoch = std::time::SystemTime::now()
+        .duration_since(std::time::SystemTime::UNIX_EPOCH)
+        .unwrap();
+    #[allow(clippy::cast_possible_truncation)]
+    let from_time = (duration_since_epoch.as_nanos() as u64).bitxor(duration_since_epoch.as_secs());
+
+    // Increment and get the global counter:
+    let from_counter = NONCE_COUNTER.fetch_add(1, Ordering::SeqCst);
+
+    // Pass these two values through the DefaultHasher, which is in itself
+    // a source of entropy (hashing only a zero should give different output
+    // one every boot):
+    let mut hasher = DefaultHasher::new();
+    from_time.hash(&mut hasher);
+    from_counter.hash(&mut hasher);
+
+    // Finally we do an XOR with time and count,
+    // just in case the DefaultHasher is broken:
+    hasher
+        .finish()
+        .bitxor(from_time)
+        .bitxor(from_counter)
+        .to_ne_bytes()
+}
+
+#[cfg(not(feature = "std"))]
+pub fn nonce_u128() -> [u8; 8] {
+    // We have no time and no hasher, so all we can do
+    // is to increment and get the global counter:
+    NONCE_COUNTER.fetch_add(1, Ordering::SeqCst).to_ne_bytes()
+}
