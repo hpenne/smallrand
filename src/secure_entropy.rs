085ae67a04cc7a1af710128d5b17c71987406f8a
Helge Penne <hpenne@gmail.com>
2025-11-24 20:49:19 +0100

    feat: Refactored SecureEntropy to support a custom failure handler.

--- src/secure_entropy.rs
+++ src/secure_entropy.rs
@@ -1,5 +1,24 @@
+//! `SecureEntropy` is an entropy source implementation that extends `EntropySource`.
+//! It leverages a [`DefaultEntropy`] source while performing additional security
+//! validations to ensure the integrity of the entropy data. These validations
+//! help identify potential failures in the entropy source.
+//!
+//! The implementation follows guidelines from the [NIST SP 800-90B] standard,
+//! specifically focusing on the Repetition Count Test and Adaptive Proportion Test.
+//!
+//! ### Features:
+//! * Performs security checks on entropy data using NIST SP 800-90B-compliant tests.
+//! * Ensures that output data does not repeat or contain previously generated sequences.
+//! * Proxy design ensures that security checks remain effective even when multiple
+//!   `SecureEntropy` instances are created.
+//!
+//! This struct provides appropriate default implementations via `new` and `default`
+//! while also enabling custom panic handling via failure handlers.
 #![cfg(feature = "std")]
+
 use crate::{DefaultEntropy, EntropySource};
+use std::convert::Infallible;
+use std::mem;
 use std::sync::{Mutex, OnceLock};
 
 /// This is an `EntropySource` (entropy source for seeds) which
@@ -11,26 +30,78 @@ use std::sync::{Mutex, OnceLock};
 /// Note that [SecureEntropy] is just a proxy for a global shared entropy source,
 /// so tests for repeats of earlier samples still work even if
 /// a new [SecureEntropy] is created for each use.
-#[derive(Default)]
-pub struct SecureEntropy;
+pub struct SecureEntropy {
+    #[allow(clippy::type_complexity)]
+    failure_handler: Option<Box<dyn FnOnce(&str) -> Infallible>>,
+}
 
 impl SecureEntropy {
-    /// Creates a new `SecureEntropy`
+    /// Creates a new `SecureEntropy`.
+    /// Calls to `fill` will panic if the entropy source fails security tests.
     ///
     /// returns: A new `SecureEntropy`
+    ///
+    /// # Panics
+    /// Panics if the entropy source fails statistics tests on creation
     #[must_use]
     pub fn new() -> Self {
-        Self {}
+        Self {
+            failure_handler: Some(Box::new(|msg| panic!("{}", msg))),
+        }
+    }
+
+    /// Creates a new `SecureEntropy` with a failure handler.
+    ///
+    /// # Arguments
+    ///
+    /// * `handler`: The failure handler.
+    ///   This function will be called if the entropy source fails security tests.
+    ///   The handler should not return,
+    ///   since the entropy source is broken and should not be used further.
+    ///
+    /// returns: A new `SecureEntropy`
+    ///
+    /// # panics
+    /// Panics if the entropy source fails statistics tests on creation
+    #[must_use]
+    pub fn new_with_failure_handler<T>(handler: T) -> Self
+    where
+        T: FnOnce(&str) -> Infallible + Sized + 'static,
+    {
+        Self {
+            failure_handler: Some(Box::new(handler)),
+        }
     }
 }
 
 impl EntropySource for SecureEntropy {
+    #[allow(unreachable_code)]
     fn fill(&mut self, destination: &mut [u8]) {
-        SECURE_ENTROPY_IMPL
-            .get_or_init(|| Mutex::new(EntropyChecker::new(DefaultEntropy::new())))
+        if let Err(msg) = SECURE_ENTROPY_IMPL
+            .get_or_init(|| {
+                Mutex::new(
+                    EntropyChecker::new(DefaultEntropy::new()).unwrap_or_else(|msg| {
+                        if let Some(handler) = mem::take(&mut self.failure_handler) {
+                            handler(msg);
+                        }
+                        unreachable!("The handler will terminate before we get here.");
+                    }),
+                )
+            })
             .lock()
             .unwrap()
-            .fill(destination);
+            .try_fill(destination)
+        {
+            if let Some(handler) = mem::take(&mut self.failure_handler) {
+                handler(msg);
+            }
+        }
+    }
+}
+
+impl Default for SecureEntropy {
+    fn default() -> Self {
+        Self::new()
     }
 }
 
@@ -50,27 +121,22 @@ impl<T> EntropyChecker<T>
 where
     T: EntropySource,
 {
-    fn new(mut wrapped_source: T) -> Self {
+    fn new(mut wrapped_source: T) -> Result<Self, &'static str> {
         let mut previous = [0; 8];
         wrapped_source.fill(&mut previous);
         let mut repetition_count_tester = RepetitionCountTester::default();
-        repetition_count_tester.test(&previous);
+        repetition_count_tester.test(&previous)?;
         let mut adaptive_proportion_tester = AdaptiveProportionTester::default();
-        adaptive_proportion_tester.test(&previous);
-        Self {
+        adaptive_proportion_tester.test(&previous)?;
+        Ok(Self {
             previous,
             entropy_source: wrapped_source,
             repetition_count_tester,
             adaptive_proportion_tester,
-        }
+        })
     }
-}
 
-impl<T> EntropySource for EntropyChecker<T>
-where
-    T: EntropySource,
-{
-    fn fill(&mut self, destination: &mut [u8]) {
+    fn try_fill(&mut self, destination: &mut [u8]) -> Result<(), &'static str> {
         // Ensure that the entropy source does not repeat itself,
         // by getting 8 bytes every time and comparing them to
         // the 8 bytes from last time.
@@ -81,31 +147,41 @@ where
         // security critical uses by the client code.
         let mut new_random = [0; 8];
         self.entropy_source.fill(&mut new_random);
-        assert_ne!(
-            new_random, self.previous,
-            "SecureEntropy: The entropy source is broken (repeats 8 byte data sequence)"
-        );
-
-        self.entropy_source.fill(destination);
-
-        // Check that the 8 bytes we fetched above are not present in the
-        // output we want to return:
-        assert!(
-            !destination
-                .windows(new_random.len())
-                .any(|candidate| candidate == new_random),
-            "SecureEntropy: The entropy source is broken (found earlier data as a substring in new data)"
-        );
+        if new_random == self.previous {
+            return Err(
+                "SecureEntropy: The entropy source is broken (repeats 8 byte data sequence)",
+            );
+        }
 
         // Run the NIST SP 800-90B "Repetition Count Test" (see section 4.4.1)
-        self.repetition_count_tester.test(&new_random);
-        self.repetition_count_tester.test(destination);
+        // on the 8 samples we fetched above:
+        self.repetition_count_tester.test(&new_random)?;
 
         // Run the NIST SP 800-90B "Adaptive Proportion Test" (see section 4.4.2)
-        self.adaptive_proportion_tester.test(&new_random);
-        self.adaptive_proportion_tester.test(destination);
+        // on the 8 samples we fetched above:
+        self.adaptive_proportion_tester.test(&new_random)?;
+
+        // Get the samples to return
+        self.entropy_source.fill(destination);
+
+        // Check that the 8 samples we fetched first are not present in the
+        // output we want to return:
+        if destination
+            .windows(new_random.len())
+            .any(|candidate| candidate == new_random)
+        {
+            return Err("SecureEntropy: The entropy source is broken (found earlier data as a substring in new data)");
+        }
+
+        // Run the NIST SP 800-90B "Repetition Count Test" (see section 4.4.1)
+        self.repetition_count_tester.test(destination)?;
+
+        // Run the NIST SP 800-90B "Adaptive Proportion Test" (see section 4.4.2)
+        self.adaptive_proportion_tester.test(destination)?;
 
         self.previous = new_random;
+
+        Ok(())
     }
 }
 
@@ -123,11 +199,11 @@ impl RepetitionCountTester {
     // four identical samples is the maximum allowed.
     const REPEAT_THRESHOLD: usize = 4;
 
-    fn test(&mut self, data: &[u8]) {
+    fn test(&mut self, data: &[u8]) -> Result<(), &'static str> {
         let mut i = data.iter();
         if self.current_value.is_none() {
             self.current_value = match i.next() {
-                None => return,
+                None => return Ok(()),
                 Some(&value) => Some(value),
             };
             self.num_found = 1;
@@ -135,15 +211,15 @@ impl RepetitionCountTester {
         for x in i {
             if Some(*x) == self.current_value {
                 self.num_found += 1;
-                assert!(
-                    self.num_found <= Self::REPEAT_THRESHOLD,
-                    "SecureEntropy: Repetition Count Test failed"
-                );
+                if self.num_found > Self::REPEAT_THRESHOLD {
+                    return Err("SecureEntropy: Repetition Count Test failed");
+                }
             } else {
                 self.current_value = Some(*x);
                 self.num_found = 1;
             }
         }
+        Ok(())
     }
 }
 
@@ -171,7 +247,7 @@ impl AdaptiveProportionTester {
     const MAX_NUM: usize = 13;
     const WINDOW_SIZE: usize = 512;
 
-    fn test(&mut self, data: &[u8]) {
+    fn test(&mut self, data: &[u8]) -> Result<(), &'static str> {
         for sample in data {
             self.state = match self.state {
                 AdaptiveProportionTesterState::InitializeWindow => Self::new_window(*sample),
@@ -184,10 +260,9 @@ impl AdaptiveProportionTester {
                         num_found += 1;
                     }
                     num_processed += 1;
-                    assert!(
-                        num_found < Self::MAX_NUM,
-                        "SecureEntropy: Adaptive Proportion Test failed"
-                    );
+                    if num_found >= Self::MAX_NUM {
+                        return Err("SecureEntropy: Adaptive Proportion Test failed");
+                    }
                     if num_processed == Self::WINDOW_SIZE {
                         AdaptiveProportionTesterState::SkipNext
                     } else {
@@ -205,6 +280,7 @@ impl AdaptiveProportionTester {
                 }
             };
         }
+        Ok(())
     }
 
     fn new_window(sample: u8) -> AdaptiveProportionTesterState {
@@ -256,21 +332,20 @@ mod tests {
             vec![
                 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
             ],
-        ]));
-        entropy_source.fill(&mut output);
+        ]))
+        .unwrap();
+        assert!(entropy_source.try_fill(&mut output).is_ok());
     }
 
     #[test]
     fn repeating_source_is_detected() {
-        let mut output = [0_u8; 8];
         let mut entropy_source = EntropyChecker::new(TestSource::new(vec![
             vec![0, 1, 2, 3, 4, 5, 6, 7],
             vec![0, 1, 2, 3, 4, 5, 6, 7],
-        ]));
-        let result = std::panic::catch_unwind(move || {
-            entropy_source.fill(&mut output);
-        });
-        assert!(result.is_err());
+        ]))
+        .unwrap();
+        let mut output = [0_u8; 16];
+        assert!(entropy_source.try_fill(&mut output).is_err());
     }
 
     #[test]
@@ -279,12 +354,10 @@ mod tests {
             vec![0, 1, 2, 3, 4, 5, 6, 7],
             vec![8, 9, 10, 11, 12, 13, 14, 15],
             vec![16, 17, 18, 19, 8, 9, 10, 11, 12, 13, 14, 15, 28, 29, 30, 31],
-        ]));
+        ]))
+        .unwrap();
         let mut output = [0_u8; 16];
-        let result = std::panic::catch_unwind(move || {
-            entropy_source.fill(&mut output);
-        });
-        assert!(result.is_err());
+        assert!(entropy_source.try_fill(&mut output).is_err());
     }
 
     #[test]
@@ -295,9 +368,10 @@ mod tests {
             vec![
                 16, 17, 18, 19, 20, 20, 20, 20, 24, 25, 26, 27, 28, 29, 30, 31,
             ],
-        ]));
+        ]))
+        .unwrap();
         let mut output = [0_u8; 16];
-        entropy_source.fill(&mut output);
+        assert!(entropy_source.try_fill(&mut output).is_ok());
     }
 
     #[test]
@@ -308,12 +382,10 @@ mod tests {
             vec![
                 16, 17, 18, 19, 20, 20, 20, 20, 20, 25, 26, 27, 28, 29, 30, 31,
             ],
-        ]));
+        ]))
+        .unwrap();
         let mut output = [0_u8; 16];
-        let result = std::panic::catch_unwind(move || {
-            entropy_source.fill(&mut output);
-        });
-        assert!(result.is_err());
+        assert!(entropy_source.try_fill(&mut output).is_err());
     }
 
     #[test]
@@ -325,12 +397,10 @@ mod tests {
                 // The previous block ends with a 15, so that makes 5 in total
                 15, 15, 15, 15, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
             ],
-        ]));
+        ]))
+        .unwrap();
         let mut output = [0_u8; 16];
-        let result = std::panic::catch_unwind(move || {
-            entropy_source.fill(&mut output);
-        });
-        assert!(result.is_err());
+        assert!(entropy_source.try_fill(&mut output).is_err());
     }
 
     #[test]
@@ -341,12 +411,10 @@ mod tests {
                 // The previous block ends with a 7, so that makes 5 in total
                 7, 7, 7, 7, 12, 13, 14, 15,
             ],
-        ]));
+        ]))
+        .unwrap();
         let mut output = [0_u8; 16];
-        let result = std::panic::catch_unwind(move || {
-            entropy_source.fill(&mut output);
-        });
-        assert!(result.is_err());
+        assert!(entropy_source.try_fill(&mut output).is_err());
     }
 
     #[test]
@@ -359,9 +427,9 @@ mod tests {
             vec3[inx] = 0;
         }
         let mut entropy_source =
-            EntropyChecker::new(TestSource::new(vec![vec1, vec2, vec3.into()]));
+            EntropyChecker::new(TestSource::new(vec![vec1, vec2, vec3.into()])).unwrap();
         let mut output = [0_u8; 512];
-        entropy_source.fill(&mut output);
+        assert!(entropy_source.try_fill(&mut output).is_ok());
     }
 
     #[test]
@@ -374,12 +442,9 @@ mod tests {
             vec3[inx] = 0;
         }
         let mut entropy_source =
-            EntropyChecker::new(TestSource::new(vec![vec1, vec2, vec3.into()]));
+            EntropyChecker::new(TestSource::new(vec![vec1, vec2, vec3.into()])).unwrap();
         let mut output = [0_u8; 512];
-        let result = std::panic::catch_unwind(move || {
-            entropy_source.fill(&mut output);
-        });
-        assert!(result.is_err());
+        assert!(entropy_source.try_fill(&mut output).is_err());
     }
 
     #[test]
@@ -392,9 +457,9 @@ mod tests {
             vec3[inx] = 0;
         }
         let mut entropy_source =
-            EntropyChecker::new(TestSource::new(vec![vec1, vec2, vec3.into()]));
+            EntropyChecker::new(TestSource::new(vec![vec1, vec2, vec3.into()])).unwrap();
         let mut output = [0_u8; 512];
-        entropy_source.fill(&mut output);
+        assert!(entropy_source.try_fill(&mut output).is_ok());
     }
 
     #[test]
@@ -415,13 +480,11 @@ mod tests {
             vec3.into(),
             vec4,
             vec5.into(),
-        ]));
+        ]))
+        .unwrap();
         let mut output1 = [0_u8; 496];
-        entropy_source.fill(&mut output1);
+        assert!(entropy_source.try_fill(&mut output1).is_ok());
         let mut output2 = [0_u8; 512];
-        let result = std::panic::catch_unwind(move || {
-            entropy_source.fill(&mut output2);
-        });
-        assert!(result.is_err());
+        assert!(entropy_source.try_fill(&mut output2).is_err());
     }
 }
